135445d9269a6e69a289b6be66223774626cc358
aliceveenathomas <aliceveenathomas@outlook.com>
2022-08-23 22:43:32 +1000

    updates

--- public/js/plotly.js
+++ public/js/plotly.js
@@ -1,4 +1,3 @@
-<<<<<<<<< Temporary merge branch 1
 async function showRatio(){
     const response = await fetch('api/tags', {
         method: 'POST',
@@ -31,31 +30,3 @@ async function showRatio(){
 
 
 showRatio()
-
-=========
-let data = [{
-    type: 'bar',
-    // x relates to y
-    x: [8, 2, 6], // data passed here
-    y: ['Arms ', 'Legs ', 'Core '],
-    orientation: 'h'
-  }];
-
-  let layout = {
-    title: "Workout Chart",
-    xaxis: {
-        range: []
-    },
-    paper_bgcolor: 'rgba(0,0,0,0)',
-    plot_bgcolor: 'rgba(0,0,0,0)',
-    font: {
-      color: 'black'
-    }
-  }
-
-  let config = {
-      responsive: true
-  }
-
-  Plotly.newPlot('workout-section', data, layout, config);
->>>>>>>>> Temporary merge branch 2
